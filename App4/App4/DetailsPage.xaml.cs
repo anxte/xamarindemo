fa5f9a6b6b9be0e07de8810033789b31d450c84c
anxte <anxte@RXSRFP4>
2016-08-24 17:28:52 +0300

    Add project files.

---
+++ App4/App4/DetailsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        async void Button_OnClicked3(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page3());
+        }
+    }
+}
---
+++ App4/App4/Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_OnClicked2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        async void Button_OnClicked4(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page4());
+        }
+    }
+}
